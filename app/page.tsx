d74e21f8beb0a32141c3209575c0604568b4e43f
cindygnaedinger <100149373+cindygnaedinger@users.noreply.github.com>
2025-02-26 16:08:22 -0300

    Modifica estilos en global.css y tailwind.config.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative">
-      <div>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="">
+        <Hero />
         <h1>Hello</h1>
       </div>
     </main>
