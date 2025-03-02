67a3644bed4745302b740233bf65521b6932a5fc
cindygnaedinger <100149373+cindygnaedinger@users.noreply.github.com>
2025-03-02 00:57:29 -0300

    Agrega seccion Testimonios

---
+++ components/Clients.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { InfiniteMovingCards } from "./ui/InfiniteMovingCards";
+import { testimonials } from "@/data";
+import MagicButton from "./ui/MagicButton";
+import { MdAdd } from "react-icons/md";
+
+const Clients = () => {
+  return (
+    <div className="py-20" id="testimonials">
+      <h1 className="heading">
+        <span className="text-purple font-bold">Palabras</span> de clientes y
+        colegas
+      </h1>
+      <p className="text-center py-5 md:tracking-wider text-sm md:text-lg max-w-2xl mx-auto">
+        Un espacio para que compartas que te pareció mi portfolio hasta ahora
+        y/o tu experiencia trabajando conmigo.
+      </p>
+      <div className="flex flex-col items-center mb-4">
+        <MagicButton
+          title="Agregar testimonio"
+          icon={<MdAdd />}
+          position="right"
+        />
+      </div>
+      <div className="flex flex-col items-center max-lg:mt-10">
+        <InfiniteMovingCards
+          items={testimonials}
+          direction="right"
+          speed="slow"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Clients;
--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -3,13 +3,13 @@ import { GlareCardDemo } from "./ui/GlareCardContainer";
 
 const RecentProjects = () => {
   return (
-    <div className="py-20">
+    <div className="py-20" id='projects'>
       <h1 className="heading">
         Algunos de mis proyectos{" "}
         <span className="text-purple font-bold">favoritos</span>
       </h1>
       <p className="text-center py-5 md:tracking-wider mb-4 text-sm md:text-lg max-w-2xl mx-auto">
-        Descubrí mis proyectos y encontrá soluciones que se adapten a tus
+        Explorá mi trabajo y encontrá soluciones que se adapten a tus
         necesidades.{" "}
         <span className="text-purple font-bold">
           ¡Tu opinión es bienvenida y me ayuda a crecer!
